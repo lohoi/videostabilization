82dc63ae341652cad131d3ee18a778ca54f7b383
skimkim <karahmkim@gmail.com>
2017-11-11 16:06:38 -0500

    feelsbadman

---
+++ experimental_files/sam_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import cv2
+import numpy as np
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture("../media/test_vid_eric.mp4")
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        print("Mission: ", ret)
+
+        # Our operations on the frame come here
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+
+        cv2.imshow('frame',frame)
+        if cv2waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
