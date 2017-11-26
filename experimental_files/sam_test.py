fe2e9edb54009661a767017be9f3770b87e728a0
Albert Lo <lohoi@umich.edu>
2017-11-25 22:53:45 -0500

    working on getting the estimate path portion working

--- experimental_files/sam_test.py
+++ experimental_files/sam_test.py
@@ -2,7 +2,6 @@
 import cv2
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     try:
         cap = cv2.VideoCapture("../media/test_vid_eric.mp4")
     except:
@@ -12,23 +11,11 @@ if __name__ == "__main__":
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
-=======
-    cap = cv2.VideoCapture("../media/drop.avi")
-    while(True):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
         if not ret:
             print("End of video.")
             break;
-            # Display the resulting frame
->>>>>>> ee42aa6925e04efa66bbfe870009fa4869ecbaad
-
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-<<<<<<< HEAD
 
-        # Display the resulting frame
-=======
->>>>>>> ee42aa6925e04efa66bbfe870009fa4869ecbaad
         cv2.imshow('frame', gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
--- project/driver.py
+++ project/driver.py
@@ -4,6 +4,14 @@ from estimate_path import *
 from smooth_path import *
 from synthesize_path import *
 
+def main():
+	# read video
+	video = read_video('/media/test_vid_eric.mp4')
+	# 1.) Estimate original camera path
+	estimate_path(video)
+	# 2.) Estimate new camera path
+	# 3.) Synthesize video with new camera path
+
 if __name__ == "__main__":
-    print("Yay")
+    main()
 
