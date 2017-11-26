27f7f99e1659c615e8a2c8405f75bf66c27c5dbe
Albert Lo <lohoi@umich.edu>
2017-11-25 20:32:08 -0500

    setting up proj

--- experimental_files/sam_test.py
+++ experimental_files/sam_test.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
-
 import cv2
-import numpy as np
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture("../media/test_vid_eric.mp4")
+    try:
+        cap = cv2.VideoCapture("../media/test_vid_eric.mp4")
+    except:
+        print "Failed opening the video"
+        raise
+
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
-        print("Mission: ", ret)
 
         # Our operations on the frame come here
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # Display the resulting frame
-
         cv2.imshow('frame', gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
