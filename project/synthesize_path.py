e80627656d34d976d2aab2fa8d812f62bcd0efe5
eyyang <eyyang@umich.edu>
2017-12-01 18:42:10 -0500

    debug synth

--- project/synthesize_path.py
+++ project/synthesize_path.py
@@ -5,10 +5,12 @@ import numpy as np
 
 def synthesize_path(vid_, p_opt):
     f_count, f_height, f_width, color_scale = vid_.shape
-
-    vid_opt = vid_;
+    vid = vid_.reshape(f_count, f_height, f_width)
+    vid_opt = vid;
     for i in range(1, f_count):
-        m_affine = p_opt[0:1, :, i]
-        warpAffine(vid_[i], vid_opt[i], p_opt[i-1], vid_opt[i].size())
+        m_affine = p_opt[i-1][0:2, :]
+        vid_opt[i] = cv2.warpAffine(vid[i], m_affine, (f_width,f_height))
+
+        # warpAffine(vid_[i], vid_opt[i], m_affine, vid_opt[i].size())
 
     return vid_opt
---
+++ project/test_synth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import numpy as np
+import skvideo.io
+
+from read_video import *
+from write_video import *
+from estimate_path import *
+from smooth_path import *
+from synthesize_path import *
+
+import pickle
+
+
+filename = '../media/test_vid_eric.mp4'
+vid = read_video(filename)
+# F, C = estimate_path(vid, method='NN')
+F = pickle.load(open("F.p", "rb"))
+C = pickle.load(open("C.p", "rb"))
+print C
+vid_opt = synthesize_path(vid, C)
+write_video('../output.mp4', vid_opt)
