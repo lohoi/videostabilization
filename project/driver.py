c9da6f3f5e11612ad72e4a7491fa4a87b3bc1b9e
Karahm Samuel Kim <karahmkim@gmail.com>
2017-12-01 23:12:38 +0000

    Allowed use of pickle instead of running alg. every time

--- project/driver.py
+++ project/driver.py
@@ -5,17 +5,24 @@ from estimate_path import *
 from smooth_path import *
 from synthesize_path import *
 
+import pickle
+
 
 def main():
     '''Main Driver.'''
 
     # read video
-    filename = '../media/test_vid_eric.mp4'
-    vid = read_video(filename)
+    #filename = '../media/test_vid_eric.mp4'
+    #vid = read_video(filename)
 
     # 1.) Estimate original camera path
-    F, C = estimate_path(vid, method='NN')
+    #F, C = estimate_path(vid, method='NN')
+
+    #pickle.dump(F, open("F.p", "wb"))
+    #pickle.dump(C, open("C.p", "wb"))
     
+    F = pickle.load(open("F.p", "rb"))
+    C = pickle.load(open("C.p", "rb"))
     # 2.) Estimate new camera path
     # 3.) Synthesize video with new camera path
     vid_opt = synthesize_path(vid, C);
