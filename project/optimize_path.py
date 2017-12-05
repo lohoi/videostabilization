e7efb8a6f571ad4d73ff52c55f8cf4f8f50311c7
eyyang <eyyang@umich.edu>
2017-12-05 16:55:10 -0500

    call matlab code from python

--- project/optimize_path.py
+++ project/optimize_path.py
@@ -13,8 +13,10 @@ def optimizePathTransforms(F, vid_shape, crop_ratio):
     with open('F.csv', 'wb') as csvfile:
         f_writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for f in F:
-            f_writer.writerow([f[0,0]] + [f[0,1]] +  [f[0,2]] + [f[0,0]] + [f[1,1]] +  [f[1,2]] + [f[2,0]] + [f[2,1]] +  [f[2,2]]) 
+            f_writer.writerow([f[0,0]] + [f[0,1]] +  [f[0,2]] + [f[0,0]] + [f[1,1]] +  [f[1,2]] + [f[2,0]] + [f[2,1]] +  [f[2,2]])
+
     # Call matlab code
+    subprocess.call("/Applications/MATLAB.app/bin/matlab -r call_optimize_transforms rm -nodisplay", shell=True)
 
     # Read resulting csv from matlab code
     p = []
@@ -26,7 +28,7 @@ def optimizePathTransforms(F, vid_shape, crop_ratio):
             for ind, val in enumerate(row):
                 p_val[int(ind / 3), ind % 3] = val
             p_val[2,2] = 1
-            p.append(p_val)    
+            p.append(p_val)
     #pickle.dump(p.p, open("optimization.p", "wb"))
     #solution = pickle.load(p..p", "rb"))
     return p
