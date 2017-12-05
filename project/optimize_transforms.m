5c3273ab9053971ae510fb36fe3b4ba9e380f6e6
eyyang <eyyang@umich.edu>
2017-12-05 16:26:44 -0500

    matlab cvx

--- project/optimize_transforms.m
+++ project/optimize_transforms.m
@@ -1,8 +1,6 @@
 function p_values = optimize_transforms(F, vid_shape, crop_ratio)
-% Number of DoF in Affine Transformation
-DoF = 6;
 % Numebr of transformations required
-f_len = vid_shape[1];
+f_len = vid_shape(1) - 1;
 % Weighting on Affine Components from Paper
 affine_weighting = [1 1 100 100 100 100];
 % Weights on different parts of path smoothing
@@ -19,7 +17,7 @@ variable e1(6,f_len)
 variable e2(6,f_len)
 variable e3(6,f_len)
 
-minimize(sum(c[1] * affine_weighting * e1 + c[2] * affine_weighting * e2 + c[3] * affine_weighting * e3));
+minimize(sum(c(1) * affine_weighting * e1 + c(2) * affine_weighting * e2 + c(3) * affine_weighting * e3));
 subject to
     % Smoothness Constraints
     for i=1:f_len - 3
@@ -28,9 +26,9 @@ subject to
         B_t2 = p_to_b_mat(p(:,i+2));
         B_t3 = p_to_b_mat(p(:,i+3));
 
-        R_t = F(i + 1) * B_t1 - B_t;
-        R_t1 = F(i + 2) * B_t2 - B_t1;
-        R_t1 = F(i + 3) * B_t3 - B_t2;
+        R_t = F(:, :, i + 1) * B_t1 - B_t;
+        R_t1 = F(:, :, i + 2) * B_t2 - B_t1;
+        R_t2 = F(:, :, i + 3) * B_t3 - B_t2;
 
         R_t = mat_to_col(R_t);
         R_t1 = mat_to_col(R_t1);
@@ -46,31 +44,35 @@ subject to
         e2(:,i) >= 0;
         e3(:,i) >= 0;
     end
+    for i=f_len-3:f_len
+        p(:,i) == p(:,f_len)
+    end
+    
     % Proximity Constraints
     lb = [0.9 0.9 -0.1 -0.1 -0.05 -0.1]';
     ub = [1.1 1.1 0.1 0.1 0.05 0.1]';
-    U_mat = [0, 0, 1, 0, 0, 0; 0, 0, 0, 0, 0, 1; 0, 0, 0, 1, 0, 0; 0, 0, 0, 0, 1, 0; 0, 0, 0, 1, 1, 0; 0, 0, 0, 1, -1, 0];
+    U = [0, 0, 1, 0, 0, 0; 0, 0, 0, 0, 0, 1; 0, 0, 0, 1, 0, 0; 0, 0, 0, 0, 1, 0; 0, 0, 0, 1, 1, 0; 0, 0, 0, 1, -1, 0];
     for i=1:f_len
-        lb <= U*p(:,i) <= ub
+        lb <= U*p(:,i) <= ub;
     end
     % Inclusion Constraints
     center = [vid_shape(3)/2 vid_shape(2)/2];
     crop_width = crop_ratio * vid_shape(3);
-    crop_height = crop_ratio * vid_shape(2)
-    corners = [center[1]-crop_width center[2]-crop_height; center[1]+crop_width center[2]-crop_height; center[1]-crop_width center[2]+crop_height; center[1]+crop_width center[2]+crop_height]; 
+    crop_height = crop_ratio * vid_shape(2);
+    corners = [center(1)-crop_width/2 center(2)-crop_height/2; center(1)+crop_width/2 center(2)-crop_height/2; center(1)-crop_width/2 center(2)+crop_height/2; center(1)+crop_width/2 center(2)+crop_height/2]; 
     for i=1:f_len
         for j=1:4
-            0 <= [1 0 corners[j,1] corners[j,2] 0 0] * p(:,i) <= vid_shape[3]
-            0 <= [0 1 0 0 corners[j,1] corners[j,2]] * p(:,i) <= vid_shape[2]
+            0 <= [1 0 corners(j,1) corners(j,2) 0 0] * p(:,i) <= vid_shape(3);
+            0 <= [0 1 0 0 corners(j,1) corners(j,2)] * p(:,i) <= vid_shape(2);
         end
     end
 cvx_end
 % end LP
-p_values = p
+p_values = p;
 end
 
 function B = p_to_b_mat(p)
-B = [p(3) p(4) p(1); p(4) p(5) p(2); 0 0 1];
+B = [p(3) p(4) p(1); p(5) p(6) p(2); 0 0 1];
 end
 
 function p = mat_to_col(R)
